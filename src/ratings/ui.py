2094288cf0e969070143a487d6c3e05651bcd895
Lars Noack <lars@webcontact.de>
2023-09-23 18:20:02 +0200

    feat: implemented ui

--- src/ratings/ui.py
+++ src/ratings/ui.py
@@ -27,18 +27,19 @@ def process_text():
 
 window = tk.Tk()
 window.title("Sentiment analysis (Man kann auch eine Excel Datei einlesen.)")
+window.configure(bg="pink")
 
 # Create a multiline text field
 text_field = scrolledtext.ScrolledText(window, wrap=tk.WORD, width=40, height=10)
 text_field.grid(row=0, column=0, padx=10, pady=10, columnspan=2, sticky="nsew")
 
-custom_text_label = tk.Label(window, text="Rating: undefined <----- JS reference")
+custom_text_label = tk.Label(window, text="Rating: undefined <----- JS reference", bg="pink")
 custom_text_label.grid(row=1, column=1, padx=10, pady=5, sticky="w")
 
 process_button = tk.Button(window, text="Process Text", command=process_text)
 process_button.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
-keyword_label = tk.Label(window, text="Keywords: undefined <----- JS reference")
+keyword_label = tk.Label(window, text="Keywords: undefined <----- JS reference", bg="pink")
 keyword_label.grid(row=2, column=0, padx=10, pady=5, columnspan=2, sticky="w")
 
 # Configure grid column weights to make the text field expand
